cd6547ba176df8e5d561bfcc6a2136e1068d2699
Ravi Malik <ravimalik2364@gmail.com>
2013-07-29 16:56:49 +0530

    Added the License Header on the source code.

--- boolean_2.c
+++ boolean_2.c
@@ -1,8 +1,29 @@
-/********************************************************************************************************************
+/*==========================================================================================
+||	
+||	Copyright (C) 2013  Ravi Malik
+||	
+||	This program is free software; you can redistribute it and/or
+||	modify it under the terms of the GNU General Public License
+||	as published by the Free Software Foundation; either version 2
+||	of the License, or (at your option) any later version.
+||	
+||	This program is distributed in the hope that it will be useful,
+||	but WITHOUT ANY WARRANTY; without even the implied warranty of
+||	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+||	GNU General Public License for more details.
+||	
+||	You should have received a copy of the GNU General Public License
+||	along with this program; if not, write to the Free Software
+||	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+||	
+||
+*==========================================================================================*/
+
+/********************************************************************************************************
 *	How to deal with boolean variables in C. Part-2.
 *	In C99 the boolean variables are introduced with an inbuilt type _Bool.
 *	It accepts only 0 or 1 as a value and trying to provide a non-zero value will result into storing 1 in it.
-*********************************************************************************************************************/
+********************************************************************************************************/
 
 #include<stdio.h>
 
--- escape_seq.c
+++ escape_seq.c
@@ -1,3 +1,24 @@
+/*==========================================================================================
+||	
+||	Copyright (C) 2013  Ravi Malik
+||	
+||	This program is free software; you can redistribute it and/or
+||	modify it under the terms of the GNU General Public License
+||	as published by the Free Software Foundation; either version 2
+||	of the License, or (at your option) any later version.
+||	
+||	This program is distributed in the hope that it will be useful,
+||	but WITHOUT ANY WARRANTY; without even the implied warranty of
+||	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+||	GNU General Public License for more details.
+||	
+||	You should have received a copy of the GNU General Public License
+||	along with this program; if not, write to the Free Software
+||	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+||	
+||
+*==========================================================================================*/
+
 /******************************************************************************************************************************************
 *	Do define an escape sequence, the number which follows the \ should be an octal or hexadecimal and should lie within the range of unsigned char.
 *	Escape sequence can be used to print charachters, and more often to print charachters that are not present on the keyboard.
